6587e8052199f08f1276817f9318a4a6ccacb6ba
etienne <etienne.lazarz@mytower.co>
2023-12-06 17:02:46 +0100

    fix norma

--- normalisation.sql
+++ normalisation.sql
@@ -1,54 +1,72 @@
--- Supprimer la table 'students_' si elle existe (à utiliser avec prudence)
-DROP TABLE IF EXISTS students_2;
+-- Drop tables if they exist (for clean setup)
+DROP TABLE IF EXISTS registrations, prefects, students, courses, houses;
 
--- Création de la table students_
-CREATE TABLE students_2(
-    id_students INT AUTO_INCREMENT PRIMARY KEY,
+
+-- Création de la table 'students'
+CREATE TABLE houses (
+    id_house INT PRIMARY KEY AUTO_INCREMENT,
+    house_name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE courses (
+    id_course INT PRIMARY KEY AUTO_INCREMENT,
+    course_name VARCHAR(255) UNIQUE
+);
+
+CREATE TABLE students (
+    id_student INT PRIMARY KEY AUTO_INCREMENT,
     student_name VARCHAR(255),
-    email VARCHAR(255),
-    year DECIMAL(10,0)
+    email VARCHAR(255) UNIQUE,
+    year DECIMAL,
+    id_house INT,
+    FOREIGN KEY (id_house) REFERENCES houses(id_house)
 );
 
--- Insertion des données dans la table students_
-INSERT INTO students_2(student_name, email, year) SELECT DISTINCT student_name, email, year FROM etudiants;
-
--- Création de la table house
-CREATE TABLE house(
-    id_house INT AUTO_INCREMENT PRIMARY KEY,
-    house_name VARCHAR(255),
-    id_students INT,
-    FOREIGN KEY (id_students) REFERENCES students_2(id_students)
+CREATE TABLE registrations (
+    id_student INT,
+    id_course INT,
+    PRIMARY KEY (id_student, id_course),
+    FOREIGN KEY (id_student) REFERENCES students(id_student),
+    FOREIGN KEY (id_course) REFERENCES courses(id_course)
 );
 
--- Insertion des données dans la table house
-INSERT INTO house(house_name, id_students)
-SELECT DISTINCT house, id_students FROM etudiants
-INNER JOIN students_2 ON etudiants.student_name = students_2.student_name;
-
--- Création de la table prefects
-CREATE TABLE prefects(
-    id_prefect INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE prefects (
+    id_prefect INT PRIMARY KEY AUTO_INCREMENT,
     prefect_name VARCHAR(255),
     id_house INT,
-    FOREIGN KEY (id_house) REFERENCES house(id_house)
+    FOREIGN KEY (id_house) REFERENCES houses(id_house)
 );
 
--- Insertion des données dans la table prefects
-INSERT INTO prefects(prefect_name, id_house)
-SELECT DISTINCT prefect, id_house FROM etudiants
-INNER JOIN house ON etudiants.house = house.house_name;
+-- Insertion des données dans les tables 'courses' et 'houses'
+INSERT INTO houses (house_name) SELECT DISTINCT house FROM etudiants;
+INSERT INTO courses (course_name) SELECT DISTINCT registered_course FROM etudiants;
 
--- Création de la table registered_courses
-CREATE TABLE registered_courses(
-    id_course INT AUTO_INCREMENT PRIMARY KEY,
-    course_name VARCHAR(255),
-    id_students INT,
-    FOREIGN KEY (id_students) REFERENCES students_2(id_students)
-);
+-- Insertion des étudiants
+INSERT INTO students (student_name, email, year, id_house)
+SELECT DISTINCT e.student_name, e.email, e.year, h.id_house
+FROM etudiants e
+JOIN houses h ON e.house = h.house_name
+ON DUPLICATE KEY UPDATE id_student = id_student;
 
--- Insertion des données dans la table registered_courses
-INSERT INTO registered_courses(course_name, id_students)
-SELECT DISTINCT registered_course, id_students FROM etudiants
-INNER JOIN students_2 ON etudiants.student_name = students_2.student_name;
 
-DROP TABLE etudiants;
+
+-- Insérer les préfets après que 'houses' ait été peuplée
+INSERT INTO prefects (prefect_name, id_house)
+SELECT 
+    DISTINCT e.prefect, 
+    h.id_house
+FROM 
+    etudiants e
+JOIN 
+    houses h ON e.house = h.house_name;
+
+-- Insérer les inscriptions après que 'students' et 'courses' aient été peuplés
+INSERT IGNORE INTO registrations (id_student, id_course)
+SELECT s.id_student, c.id_course
+FROM etudiants e
+JOIN students s ON e.student_name = s.student_name AND e.email = s.email
+JOIN courses c ON e.registered_course = c.course_name;
+
+
+-- Supprimer la table 'etudiants' si tout est correct
+-- DROP TABLE etudiants;
--- vues.sql
+++ vues.sql
@@ -1,31 +1,60 @@
 USE project;
 
 -- a. Créer une vue logique qui affiche le nom, l'email et la maison de chaque étudiant qui suit un cours de potions
-CREATE VIEW potion_students_view AS SELECT student_name, email, house FROM etudiants WHERE registered_course = 'potion';
+DROP VIEW IF EXISTS view_students_potions;
+
+
+CREATE VIEW view_students_potions AS 
+SELECT s.student_name, s.email, h.house_name
+FROM students s
+JOIN registrations r ON s.id_student = r.id_student
+JOIN courses c ON r.id_course = c.id_course
+JOIN houses h ON s.id_house = h.id_house
+WHERE c.course_name = 'potion';
+
+
+
 
 -- b. Afficher le résultat de la vue.
-SELECT * FROM potion_students_view;
+SELECT * FROM view_students_potions;
 
 -- c. Rajouter 2 étudiants qui suivent un cours de potion.
 
--- TODO
+-- Ajouter les étudiants
+INSERT INTO students (student_name, email, year) VALUES 
+('Etudiant1', 'etudiant1@poudlard.edu', 1),
+('Etudiant2', 'etudiant2@poudlard.edu', 1);
 
+-- Récupérer l'ID du cours de potion
+SET @course_id_potion = (SELECT id_course FROM courses WHERE course_name = 'potion');
 
-INSERT INTO etudiants (student_name, email, registered_course, year, house, prefect)
-VALUES ('Marie', 'marie@poudlard.edu', 'potion', 3.0, 'Gryffondor', 'Godrick'),
-       ('Loic', 'loic@poudlard.edu', 'potion', 1.0, 'Serdaigle', 'Help');
+-- Récupérer les ID des étudiants nouvellement ajoutés en supposant que les emails sont uniques
+SET @student_id_1 = (SELECT id_student FROM students WHERE email = 'etudiant1@poudlard.edu');
+SET @student_id_2 = (SELECT id_student FROM students WHERE email = 'etudiant2@poudlard.edu');
+
+-- Ajouter leurs inscriptions au cours de potion
+INSERT INTO registrations (id_student, id_course) VALUES 
+(@student_id_1, @course_id_potion),
+(@student_id_2, @course_id_potion);
+
 
 -- d. Afficher (encore) le résultat de la vue.
-SELECT * FROM potion_students_view;
--- vérifie que marie et loic sont insérés
-SELECT * FROM potion_students_view WHERE student_name = "Marie" OR student_name = "Loic";
+SELECT * FROM view_students_potions;
 
 
 -- 3. Modification interdite d'une vue :
 
 -- Créer une vue house_student_count qui regroupe les étudiants par maison et compte le nombre d'étudiants dans chaque maison.
-CREATE VIEW house_student_count AS SELECT house AS house_name, COUNT(*) AS student_count FROM etudiants GROUP BY house;
+CREATE VIEW house_student_count AS
+SELECT houses.house_name, COUNT(registrations.id_student) AS student_count
+FROM houses
+JOIN students ON houses.id_house = students.id_student
+JOIN registrations ON students.id_student = registrations.id_student
+GROUP BY houses.house_name;
+
+
+
 
 -- Essayer de modifier la colonne contenant le nombre d'étudiants dans une maison. Par exemple, pour la maison Gryffondor, définir le nombre d'étudiants à 10.
-
 UPDATE house_student_count SET student_count = 10 WHERE house_name = 'Gryffondor';
+
