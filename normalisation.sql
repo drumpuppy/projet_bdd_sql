501cbb7bf8909b657602abe0b749cab7679c2ec3
etienne <etienne.lazarz@mytower.co>
2023-12-06 13:44:00 +0100

    normalisation

--- normalisation.sql
+++ normalisation.sql
@@ -1,8 +1,8 @@
 -- Supprimer la table 'students_' si elle existe (à utiliser avec prudence)
-DROP TABLE IF EXISTS students_;
+DROP TABLE IF EXISTS students_2;
 
 -- Création de la table students_
-CREATE TABLE students_(
+CREATE TABLE students_2(
     id_students INT AUTO_INCREMENT PRIMARY KEY,
     student_name VARCHAR(255),
     email VARCHAR(255),
@@ -10,21 +10,20 @@ CREATE TABLE students_(
 );
 
 -- Insertion des données dans la table students_
-INSERT INTO students_(student_name, email, year)
-SELECT DISTINCT student_name, email, year FROM etudiants;
+INSERT INTO students_2(student_name, email, year) SELECT DISTINCT student_name, email, year FROM etudiants;
 
 -- Création de la table house
 CREATE TABLE house(
     id_house INT AUTO_INCREMENT PRIMARY KEY,
     house_name VARCHAR(255),
     id_students INT,
-    FOREIGN KEY (id_students) REFERENCES students_(id_students)
+    FOREIGN KEY (id_students) REFERENCES students_2(id_students)
 );
 
 -- Insertion des données dans la table house
 INSERT INTO house(house_name, id_students)
 SELECT DISTINCT house, id_students FROM etudiants
-INNER JOIN students_ ON etudiants.student_name = students_.student_name;
+INNER JOIN students_2 ON etudiants.student_name = students_2.student_name;
 
 -- Création de la table prefects
 CREATE TABLE prefects(
@@ -44,12 +43,12 @@ CREATE TABLE registered_courses(
     id_course INT AUTO_INCREMENT PRIMARY KEY,
     course_name VARCHAR(255),
     id_students INT,
-    FOREIGN KEY (id_students) REFERENCES students_(id_students)
+    FOREIGN KEY (id_students) REFERENCES students_2(id_students)
 );
 
 -- Insertion des données dans la table registered_courses
 INSERT INTO registered_courses(course_name, id_students)
 SELECT DISTINCT registered_course, id_students FROM etudiants
-INNER JOIN students_ ON etudiants.student_name = students_.student_name;
+INNER JOIN students_2 ON etudiants.student_name = students_2.student_name;
 
 DROP TABLE etudiants;
